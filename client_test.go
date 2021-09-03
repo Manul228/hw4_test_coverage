5e3e17632a7360967e12060857b787c125caff76
Manul228 <Manul228@users.noreply.github.com>
2021-09-03 17:06:42 +0300

    done

--- client_test.go
+++ client_test.go
@@ -3,11 +3,15 @@ package main
 import (
 	"encoding/json"
 	"encoding/xml"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -75,11 +79,40 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	limit, err := strconv.Atoi(r.FormValue("limit"))
+	if err != nil {
+		msg := fmt.Sprintf("Error when parsing limit: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	offset, err := strconv.Atoi(r.FormValue("offset"))
+	if err != nil {
+		msg := fmt.Sprintf("Error when parsing offset: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(io.Discard, "offset: %v\n", offset)
+
 	query := r.FormValue("query")
+	orderField := r.FormValue("order_field")
+
+	fmt.Fprintf(io.Discard, "orderField: %v\n", orderField)
+
+	orderBy, err := strconv.Atoi(r.FormValue("order_by"))
+	if err != nil {
+		msg := fmt.Sprintf("Error when parsing order_by: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(io.Discard, "orderBy: %v\n", orderBy)
+
 	var result []User
 	for _, row := range dataset.Row {
 		name := row.FirstName + " " + row.LastName
-		if strings.Contains(name, query) {
+		if strings.Contains(fmt.Sprint(row), query) {
 			result = append(result, User{
 				Id:     row.ID,
 				Name:   name,
@@ -90,17 +123,73 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	switch orderField {
+	case "Id":
+		if orderBy == OrderByAsc {
+			sort.SliceStable(result, func(p, q int) bool {
+				return result[p].Id < result[q].Id
+			})
+		}
+
+		if orderBy == OrderByDesc {
+			sort.SliceStable(result, func(p, q int) bool {
+				return result[p].Id > result[q].Id
+			})
+		}
+	case "Name", "":
+		if orderBy == OrderByAsc {
+			sort.SliceStable(result, func(p, q int) bool {
+				return result[p].Name < result[q].Name
+			})
+		}
+
+		if orderBy == OrderByDesc {
+			sort.SliceStable(result, func(p, q int) bool {
+				return result[p].Name > result[q].Name
+			})
+		}
+
+	case "Age":
+		if orderBy == OrderByAsc {
+			sort.SliceStable(result, func(p, q int) bool {
+				return result[p].Age < result[q].Age
+			})
+		}
+
+		if orderBy == OrderByDesc {
+			sort.SliceStable(result, func(p, q int) bool {
+				return result[p].Age > result[q].Age
+			})
+		}
+	default:
+		message, _ := json.Marshal(SearchErrorResponse{Error: "ErrorBadOrderField"})
+		http.Error(w, string(message), http.StatusBadRequest)
+		return
+	}
+
+	if offset > len(result) {
+		offset = len(result)
+	}
+
+	if limit == 0 {
+		limit = len(result)
+	}
+
+	if offset+limit > len(result) {
+		limit = len(result) - offset
+	}
+
+	result = result[offset : limit+offset]
+
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	enc := json.NewEncoder(w)
-	err := enc.Encode(result)
+	err = enc.Encode(result)
 
 	if err != nil {
 		panic(err)
 	}
 
-	// http.Error(w, "Unauthorized", http.StatusUnauthorized)
-	// return
 }
 
 func TestFindUsers(t *testing.T) {
@@ -168,11 +257,11 @@ func TestFindUsers(t *testing.T) {
 				Limit:      1000,
 				Offset:     0,
 				Query:      "",
-				OrderField: "",
+				OrderField: "jjjj",
 				OrderBy:    0,
 			},
-			IsError:      false,
-			ErrorMessage: "",
+			IsError:      true,
+			ErrorMessage: "bad orderfield",
 		},
 	}
 
@@ -204,7 +293,7 @@ func TestFindUsersStatusUnauthorized(t *testing.T) {
 			OrderBy:    OrderByAsc,
 		},
 		IsError:      true,
-		ErrorMessage: "",
+		ErrorMessage: "no token",
 	}
 
 	client := &SearchClient{
@@ -215,7 +304,7 @@ func TestFindUsersStatusUnauthorized(t *testing.T) {
 	_, err := client.FindUsers(tc.Request)
 
 	if err == nil {
-		t.Errorf("Unexpected success with auth")
+		t.Errorf(tc.ErrorMessage)
 	}
 }
 
@@ -307,6 +396,68 @@ func TestFindUsersUnknownError(t *testing.T) {
 	client := &SearchClient{
 		AccessToken: "kek",
 		URL:         "ftp://192.168.1.1",
+	}
+
+	_, err := client.FindUsers(tc.Request)
+
+	if err == nil {
+		t.Errorf(tc.ErrorMessage)
+	}
+}
+
+func TestFindUsersStatusBadResuestErrorJSON(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, "badJSON", http.StatusBadRequest)
+	}))
+	defer ts.Close()
+
+	tc := TestCase{
+		Request: SearchRequest{
+			Limit:      12,
+			Offset:     0,
+			Query:      "dddd",
+			OrderField: "dfg",
+			OrderBy:    OrderByDesc,
+		},
+		IsError:      true,
+		ErrorMessage: "bad json in error while ordering",
+	}
+
+	client := &SearchClient{
+		AccessToken: "kek",
+		URL:         ts.URL,
+	}
+
+	_, err := client.FindUsers(tc.Request)
+
+	if err == nil {
+		t.Errorf(tc.ErrorMessage)
+	}
+}
+
+func TestFindUsersStatusBadResuestError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		message, _ := json.Marshal(SearchErrorResponse{Error: "LOLKEK"})
+		http.Error(w, string(message), http.StatusBadRequest)
+	}))
+	defer ts.Close()
+
+	tc := TestCase{
+		Request: SearchRequest{
+			Limit:      12,
+			Offset:     0,
+			Query:      "dddd",
+			OrderField: "dfg",
+			OrderBy:    OrderByDesc,
+		},
+		IsError:      true,
+		ErrorMessage: "unknown error while ordering",
+	}
+
+	client := &SearchClient{
+		AccessToken: "kek",
+		URL:         ts.URL,
 	}
 
 	_, err := client.FindUsers(tc.Request)
