104a2b1bf6e894596c214ab52956efbd97761877
Manul228 <Manul228@users.noreply.github.com>
2021-09-03 11:12:05 +0300

    ready to start

--- client_test.go
+++ client_test.go
@@ -1,3 +1,33 @@
 package main
 
-// код писать тут
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SearchServer(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	return
+}
+
+func TestFindUsers(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer ts.Close()
+
+	request := SearchRequest{
+		Limit:      12,
+		Offset:     0,
+		Query:      "Dillard",
+		OrderField: "Id",
+		OrderBy:    OrderByAsc,
+	}
+	client := &SearchClient{
+		AccessToken: "lol",
+		URL:         ts.URL,
+	}
+
+	result, err := client.FindUsers(request)
+
+	t.Error(err, result)
+}
